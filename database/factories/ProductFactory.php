28c7c675d6ab2ccb30c8c8bd8e3f7c4d6fe6b2fa
ly pengleang <lypengleang@gmail.com>
2020-04-24 16:51:33 +0700

    Seeding part for Produts and Reviews

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        /**$table->string('name');
+                $table->text('detail');
+                $table->integer('price');
+                $table->integer('stock'); $table->integer('discount');*/
+            'name'=>$faker->word,
+            'detail'=>$faker->paragraph,
+            'price'=>$faker->numberBetween(100,1000),
+            'stock'=>$faker->randomDigit,
+            'discount'=>$faker->numberBetween(2,30),
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,19 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Product;
 use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        /**$table->string('customer'); $table->text('review');
+            $table->integer('star'); */
+            'product_id'=>function(){
+                return Product::all()->random();
+            },
+            'customer'=>$faker->name,
+            'review'=>$faker->paragraph,
+            'star'=>$faker->numberBetween(0,5)
     ];
 });
