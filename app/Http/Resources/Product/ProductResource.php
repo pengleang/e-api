056a3afc247c96543d4d40d932a5296c9e511302
ly pengleang <lypengleang@gmail.com>
2020-04-25 15:49:08 +0700

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,8 +20,13 @@ class ProductResource extends JsonResource
             'description'=>$this->detail,
             'price'=>$this->price,
             'stock'=>$this->stock,
-            'discount'=>$this->discount
-
+            'discount'=>$this->discount,
+            'totalPrice'=>round((1-($this->discount/100))*$this->price,2), //23/100=.23 then 1-.23=.77 then .77*475=365.75
+            //'rating'=>round($this->reviews->sum('star') / $this->reviews->count()), //avg=sum/total
+            'rating'=>$this->reviews->count()>0 ? round($this->reviews->sum('star') / $this->reviews->count(),2): 'No rating yet',
+            'href'=>[
+                'reviews'=> route('reviews.index',$this->id)
+            ]
         ];
     }
 }
