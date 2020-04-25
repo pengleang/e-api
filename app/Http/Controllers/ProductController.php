6d29e12103045b3cbfacff31485092d3e1f79968
ly pengleang <lypengleang@gmail.com>
2020-04-25 21:15:01 +0700

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
@@ -15,8 +16,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-
-        Return Product::all();
+      // Return ProductResource::collection(Product::all());
+      // return new ProductCollection(Product::all());
+      return ProductCollection::collection(Product::all());
     }
 
     /**
